bf846ba4d1c9353229d041a17e641a191d61e713
Avrumi Friedman <info@lakewoodprogramming.com>
2019-04-10 21:53:05 -0400

    Initial commit

---
+++ AjaxPeople.Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxPeople.Data;
+using System.Threading;
+
+namespace AjaxPeople.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            Thread.Sleep(3000);
+            PersonDb db = new PersonDb(Properties.Settings.Default.ConStr);
+            return Json(db.GetPeople(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Person person)
+        {
+            PersonDb db = new PersonDb(Properties.Settings.Default.ConStr);
+            db.AddPerson(person);
+            return Json(person);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Person person)
+        {
+            PersonDb db = new PersonDb(Properties.Settings.Default.ConStr);
+            db.Update(person);
+            return Json(person);
+        }
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+            PersonDb db = new PersonDb(Properties.Settings.Default.ConStr);
+            db.Delete(id);
+        }
+
+    }
+}
---
+++ AjaxPeople.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxPeople.Web.MvcApplication" Language="C#" %>
